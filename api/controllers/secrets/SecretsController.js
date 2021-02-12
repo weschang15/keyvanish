ca67010530d07893e93c5b6a05d0a6e56c1fd7d7
Wesley Chang <weschang15@gmail.com>
2021-02-12 13:56:42 -0800

    feat: add boilerplate controller

---
+++ api/controllers/secrets/SecretsController.js
@@ -0,0 +1,7 @@
+const SecretsController = {};
+
+SecretsController.createSecret = function (req, res) {};
+
+SecretsController.getSecret = function (req, res) {};
+
+module.exports = SecretsController;
