54c64a44bfaa69b7ceb5e41b8c9319738402b005
Wesley Chang <weschang15@gmail.com>
2021-02-16 20:43:36 -0800

    feat: add toJSON method

--- models/Secret.js
+++ models/Secret.js
@@ -1,27 +1,41 @@
 const { createModel, createSchema } = require("./utils");
 const { getMsFromMins } = require("../shared/utils/time");
 
-const schema = createSchema({
-  content: {
-    type: String,
-    required: true,
-    trim: true,
+const schema = createSchema(
+  {
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      minlength: 8,
+      required: true,
+    },
+    expiration: {
+      type: Date,
+      default: () => Date.now() + getMsFromMins(5),
+      required: true,
+    },
+    used: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    trim: true,
-    min: [8, "Password must be at least 8 characters long."],
-    required: true,
-  },
-  expiration: {
-    type: Date,
-    default: () => Date.now() + getMsFromMins(5),
-    required: true,
-  },
-  used: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { toJSON: { getters: true, virtuals: false } }
+);
+
+schema.methods.toJSON = function () {
+  return {
+    _id: this._id,
+    content: this.content,
+    expiration: this.expiration,
+    used: this.used,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
+  };
+};
 
 module.exports = createModel("Secret", schema);
