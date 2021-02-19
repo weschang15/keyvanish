9566e0124c4e211d8b2caa05afc75148d73aca0f
Wesley Chang <weschang15@gmail.com>
2021-02-19 12:29:07 -0800

    feat: execute query

--- shared/jobs/expireSecrets.js
+++ shared/jobs/expireSecrets.js
@@ -3,6 +3,6 @@ const { Secret } = require("../../models");
 module.exports = (agenda) => {
   agenda.define("expire secret message", async (job) => {
     const { secretId } = job.attrs.data;
-    await Secret.findByIdAndDelete(secretId);
+    await Secret.findByIdAndDelete(secretId).exec();
   });
 };
