9ff242759baf2090ebf0d46e7384f7d9e475d66a
Wesley Chang <weschang15@gmail.com>
2021-03-11 14:18:33 -0800

    feat: configure redis store for rate limiting

--- api/middlewares/rateLimiter.js
+++ api/middlewares/rateLimiter.js
@@ -1,7 +1,13 @@
 const rateLimit = require("express-rate-limit");
+const RedisStore = require("rate-limit-redis");
+const { createRedis } = require("../../shared/services/redis");
 const { getMsFromMins } = require("../../shared/utils/time");
+const client = createRedis();
 
 exports.rateLimiter = rateLimit({
+  store: new RedisStore({
+    client,
+  }),
   windowMs: getMsFromMins(1),
   max: 10,
 });
