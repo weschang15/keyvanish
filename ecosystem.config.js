9fc7d87056ff534093808970240c8978fa0549f0
Wesley Chang <weschang15@gmail.com>
2021-03-12 13:25:28 -0800

    fix: switch to fork mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      exec_mode: "fork",
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "development",
@@ -22,6 +23,7 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
+      exec_mode: "fork",
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "development",
