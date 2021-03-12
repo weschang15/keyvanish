c346e762a7b3da1b740888dcec1570bd8c849caa
Wesley Chang <weschang15@gmail.com>
2021-03-12 08:58:44 -0800

    feat: setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps: [
+    {
+      name: "Worker",
+      script: "./worker.js",
+      node_args: "-r dotenv/config",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "API",
+      script: "./start.js",
+      node_args: "-r dotenv/config",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
